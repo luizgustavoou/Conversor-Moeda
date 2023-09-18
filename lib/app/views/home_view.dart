da09fca302ee428849018aa200a742dc068af8b7
Luiz Gustavo de Oliveira Umbelino <luiz.umbelino@nuplam.ufrn.br>
2023-09-18 12:56:53 -0300

    alinhar os itens da view

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -9,24 +9,64 @@ class HomeView extends StatelessWidget {
         body: SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          Text('Hello world!'),
-          Image.asset('assets/images/logo.png'),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              children: [
-                Expanded(
-                    child: DropdownButton(items: [], onChanged: (value) {})),
-                Expanded(
-                    child: TextField(
-                        onChanged: (text) {}, decoration: InputDecoration())),
-              ],
+      child: Padding(
+        padding:
+            const EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 20),
+        child: Column(
+          children: [
+            Text('Hello world!'),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 130,
+              height: 130,
             ),
-          )
-          // ElevatedButton(onPressed: () {}, child: Text('CONVERTER'))
-        ],
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: SizedBox(
+                        height: 56,
+                        child: DropdownButton(
+                            isExpanded: true,
+                            underline: Container(
+                              height: 1,
+                              color: Colors.amber,
+                            ),
+                            value: 'a',
+                            items: [
+                              DropdownMenuItem(value: 'a', child: Text('Real')),
+                              DropdownMenuItem(value: 'b', child: Text('Dolar'))
+                            ],
+                            onChanged: (value) {}),
+                      )),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: TextField(
+                          onChanged: (text) {},
+                          decoration: InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.amber)),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.amber))))),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+                onPressed: () {},
+                child: Text('CONVERTER'))
+          ],
+        ),
       ),
     ));
   }
